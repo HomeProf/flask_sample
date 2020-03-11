f7ad103f9f63689b83cb4b804e08d9c5dd3a8aab
Benedikt Hofrichter <hofrichter.consult@gmail.com>
2020-03-11 22:25:58 +0100

    config and login form added.

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template
+from config import Config
 
 flask_engine = Flask(__name__)
+flask_engine.config.from_object(Config)
 
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,22 @@
 from app import flask_engine
-from flask import render_template
+from flask import render_template, flash, redirect
+from app.forms import LoginForm
+
+
+@flask_engine.route('/config')
+def config_test():
+    x = flask_engine.config['SECRET_KEY']
+
+    return render_template('config.html', x=x)
+
+@flask_engine.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+        form.username.data, form.remember_me.data))
+        return redirect('/index')
+    return render_template('login.html', title='Sign In', form=form)
 
 @flask_engine.route('/')
 @flask_engine.route('/index')
