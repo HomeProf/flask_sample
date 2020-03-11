05f099f3729bca353c90b1df766a4df73ca049a8
Benedikt Hofrichter <hofrichter.consult@gmail.com>
2020-03-11 17:28:26 +0100

    loop integration

--- app/routes.py
+++ app/routes.py
@@ -5,4 +5,18 @@ from flask import render_template
 @flask_engine.route('/index')
 def abc():
     user = {'username': 'Miguel'}
-    return render_template('index.html', title='Home', user=user)
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Some nice day here!'
+        },
+        {
+            'author': {'username': 'Max'},
+            'body': 'I want to also say something.'
+        },
+        {
+            'author': {'username': 'TomTom'},
+            'body': 'I am not a navigator!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
