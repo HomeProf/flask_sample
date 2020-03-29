8ecc517c87dc21f82cf236958100a38094372bf6
Benedikt Hofrichter <baghofrichter@gmail.com>
2020-03-29 22:55:23 +0200

    Bugfix

--- app/models.py
+++ app/models.py
@@ -4,6 +4,13 @@ from app import db, login
 from flask_login import UserMixin
 from hashlib import md5
 
+
+# auxiliary table
+followers = db.Table('followers',
+        db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
+        db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
+)
+
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
@@ -11,12 +18,6 @@ class User(UserMixin, db.Model):
     password_hash = db.Column(db.String(128))
     about_me = db.Column(db.String(140))
     last_seen = db.Column(db.DateTime, default=datetime.utcnow())
-
-    # auxiliary table
-    followers = db.Table('followers',
-        db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
-        db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
-    )
 
     # relationships
     posts = db.relationship('Post', backref='author', lazy='dynamic')
@@ -40,19 +41,19 @@ class User(UserMixin, db.Model):
         return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(digest, size)
 
     def follow(self, user):
-        if not self.is_follwing(user):
+        if not self.is_following(user):
             self.followed.append(user)
 
     def unfollow(self, user):
-        if self.is_follwing(user):
+        if self.is_following(user):
             self.followed.remove(user)
 
-    def is_follwing(self, user):
+    def is_following(self, user):
         return self.followed.filter(
             followers.c.followed_id == user.id).count() > 0
 
     def followed_posts(self):
-        followed = Post.qury.join(followers, (followers.c.follwed_id == Post.user_id)).filter(
+        followed = Post.query.join(followers, (followers.c.followed_id == Post.user_id)).filter(
             followers.c.follower_id == self.id)
         own = Post.query.filter_by(user_id=self.id)
         return followed.union(own).order_by(Post.timestamp.desc())
